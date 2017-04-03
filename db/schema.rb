3bfc8f1da8b014e6660fde9fe0b882749c4d195d
emlai <emil@cs.helsinki.fi>
2017-04-03 17:23:03 +0300

    Generate Article scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170403141839) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "author"
+    t.string   "title"
+    t.integer  "year"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
