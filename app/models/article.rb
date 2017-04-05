e4a58d2ab5873544b43b04cc8d4262a206f84403
Enzine <sonja.makipaja@kolumbus.fi>
2017-04-05 16:50:56 +0300

    updates to article

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,6 @@ class Article < ApplicationRecord
   validates :author, presence: true, allow_blank: false
   validates :title, presence: true, allow_blank: false
   validates :year, presence: true, allow_blank: false
-  validates :journal, presence: true, allow_blank: false
-  validates :volume, presence: true, allow_blank: false
-
+  validates :journal, presence: true
+  validates :volume, presence: true
 end
