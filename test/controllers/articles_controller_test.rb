11aff726169c0e959838c4231a912bbc241f51b3
Enzine <sonja.makipaja@kolumbus.fi>
2017-04-05 17:09:49 +0300

    fixed broken tests

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -17,7 +17,11 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
 
   test "should create article" do
     assert_difference('Article.count') do
-      post articles_url, params: { article: { author: @article.author, title: @article.title, year: @article.year } }
+      post articles_url, params: { article: { author: @article.author,
+                                              title: @article.title,
+                                              year: @article.year,
+                                              journal: @article.journal,
+                                              volume: @article.volume } }
     end
 
     assert_redirected_to article_url(Article.last)
@@ -34,7 +38,11 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update article" do
-    patch article_url(@article), params: { article: { author: @article.author, title: @article.title, year: @article.year } }
+    patch article_url(@article), params: { article: { author: @article.author,
+                                                      title: @article.title,
+                                                      year: @article.year,
+                                                      journal: @article.journal,
+                                                      volume: @article.volume } }
     assert_redirected_to article_url(@article)
   end
 
