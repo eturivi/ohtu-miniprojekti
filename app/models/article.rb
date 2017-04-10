aa567b5ef30b964e532c59a83994d68d3276be55
emlai <emil@cs.helsinki.fi>
2017-04-10 18:27:14 +0300

    Make Article volume optional
    It's optional according to the BibTeX spec:
    http://newton.ex.ac.uk/tex/pack/bibtex/btxdoc/node6.html

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,5 +3,4 @@ class Article < ApplicationRecord
   validates :title, presence: true, allow_blank: false
   validates :year, presence: true, allow_blank: false
   validates :journal, presence: true
-  validates :volume, presence: true
 end
