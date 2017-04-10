48112602716693e4e7f02717666838f6a95fa4ab
emlai <emil@cs.helsinki.fi>
2017-04-10 18:34:21 +0300

    [Cucumer] Add journal to make tests pass again

--- features/step_definitions/reference_steps.rb
+++ features/step_definitions/reference_steps.rb
@@ -2,10 +2,12 @@ Given(/^I am at (.*)$/) do |page_path|
   visit page_path
 end
 
-Given(/^I have filled in author as "(.*)", title as "(.*)", year as "(.*)"$/) do |author, title, year|
+Given(/^I have filled in author as "(.*)", title as "(.*)", year as "(.*)", journal as "(.*)"$/) do
+  |author, title, year, journal|
   fill_in "Author", with: author
   fill_in "Title", with: title
   fill_in "Year", with: year
+  fill_in "Journal", with: journal
 end
 
 When(/^I click "(.*)"$/) do |link|
