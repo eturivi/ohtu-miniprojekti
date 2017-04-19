6753eaaad19da4ae62e81bca81858188f0b27c9b
Ilja Häkkinen <ilja.hakkinen@helsinki.fi>
2017-04-19 15:38:58 +0300

    cucumber tests updated

---
+++ features/articles_humanreadable.feature
@@ -0,0 +1,6 @@
+Feature: As a customer I can see a list of references in human readable form
+
+  Scenario:
+    Given I am at front page
+    When  I click "Show"
+    Then  the information is shown in human readable form
---
+++ features/step_definitions/articles_steps.rb
@@ -0,0 +1,40 @@
+Given(/^new article is selected$/) do
+  visit 'articles/new'
+end
+
+Given(/^I am at (.*)$/) do |path|
+  visit path == "front page" ? '/' : path
+end
+
+When(/^author "(.*)", title "(.*)", year "(.*)", journal "(.*)" are given$/) do
+  |author, title, year, journal|
+  fill_in "Author", with: author
+  fill_in "Title", with: title
+  fill_in "Year", with: year
+  fill_in "Journal", with: journal
+  click_button 'Create Article'
+end
+
+When(/^I click "(.*)"$/) do |link|
+  click_link link
+end
+
+Then(/^article is added$/) do
+  assert page.has_content?('Article was successfully created.')
+end
+
+Then(/^scandics are shown properly$/) do
+  assert page.has_content?('Ääöå')
+end
+
+Then(/^article is not added and error message is given$/) do
+  assert page.has_content?('1 error prohibited this article from being saved')
+end
+
+Then(/^the information is shown in human readable form$/) do
+  assert page.has_content?('Author: test user åäö')
+  assert page.has_content?('Title: test article')
+  assert page.has_content?('Year: 2017')
+  assert page.has_content?('Journal: test journal')
+  assert page.has_content?('Volume: 189')
+end
