98f033a4807f601bc9b430534756e93c1359f408
Enzine <sonja.makipaja@kolumbus.fi>
2017-04-20 15:01:37 +0300

    fixed some tests

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ApplicationRecord
   has_many :taggings
   has_many :posts, through: :taggings
-  validates :name, presence: true
+  validates :name, presence: true, allow_blank: false
 end
