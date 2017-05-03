c1612d1f28df652dcd615a613664df2be7662ad6
Ilja Häkkinen <ilja.hakkinen@helsinki.fi>
2017-05-03 22:15:09 +0300

    controller test for selected download

--- test/controllers/references_controller_test.rb
+++ test/controllers/references_controller_test.rb
@@ -1,6 +1,36 @@
 require 'test_helper'
 
 class ReferencesControllerTest < ActionDispatch::IntegrationTest
+  test ".bib file download with selected references works properly" do
+    expected = <<~END
+      @article{tes20171,
+        author = "test user åäö",
+        title = "test article",
+        year = "2017",
+        journal = "test journal",
+        volume = "189" }
+
+      @inproceeding{MyA18232,
+        author = "MyAuthor",
+        title = "MyTitle",
+        booktitle = "MyBooktitle",
+        year = "1823",
+        editor = "MyEditor",
+        volume = "4",
+        series = "MySeries",
+        pages = "MyPages",
+        address = "MyAddress",
+        month = "7",
+        organization = "MyOrganization",
+        publisher = "MyPublisher",
+        note = "MyNote" }\n
+        END
+
+    post alldownload_selected_path, params: {"Inproceedingselected" => ["1"], "Articleselected" => ["24"]}
+    assert_equal expected, response.body
+    assert_response 200
+  end
+
   test ".bib file download with multiple references works properly" do
     expected = <<~END
       @article{tes20171,
